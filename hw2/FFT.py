1139578fc685f8801aa6a8f098aafdf8ecf01a32
huangfonglee <huang.fong.lee666@gmail.com>
2021-03-30 06:48:23 -0700

    add FFT.py

--- hw2/FFT.py
+++ hw2/FFT.py
@@ -3,10 +3,14 @@ import numpy as np
 import serial
 import time
 
-Fs = 100.0;  # sampling rate
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev)
+
+freq = int(s.readline())  # sampling rate
+Fs = freq * 50
 Ts = 1.0/Fs; # sampling interval
-t = np.arange(0,1,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
-y = np.arange(0,1,Ts) # signal vector; create Fs samples
+t = np.arange(0,1 / freq * 4,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
+y = np.arange(0,1 / freq * 4,Ts) # signal vector; create Fs samples
 
 n = len(y) # length of the signal
 k = np.arange(n)
@@ -14,9 +18,8 @@ T = n/Fs
 frq = k/T # a vector of frequencies; two sides frequency range
 frq = frq[range(int(n/2))] # one side frequency range
 
-serdev = '/dev/ttyACM0'
-s = serial.Serial(serdev)
-for x in range(0, int(Fs)):
+
+for x in range(0, int(200)):
     line=s.readline() # Read an echo string from B_L4S5I_IOT01A terminated with '\n'
     # print line
     y[x] = float(line)
@@ -29,6 +32,7 @@ ax[0].plot(t,y)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
 ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
+ax[1].set_xlim([0, 1800])
 ax[1].set_xlabel('Freq (Hz)')
 ax[1].set_ylabel('|Y(freq)|')
 plt.show()
